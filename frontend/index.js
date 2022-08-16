2b36b8c5ebf549497358536a7861b69c00d17d43
DZykov <dzykov@protonmail.com>
2022-08-15 20:43:16 -0400

    add projectiles

--- frontend/index.js
+++ frontend/index.js
@@ -6,12 +6,12 @@ canvas.width = innerWidth;
 canvas.height = innerHeight;
 
 class Player {
-    constructor(x, y, radius, colour, speed) {
+    constructor(x, y, radius, colour, velocity) {
         this.x = x;
         this.y = y;
         this.radius = radius;
         this.colour = colour;
-        this.speed = speed;
+        this.velocity = velocity;
     }
 
     draw() {
@@ -22,8 +22,61 @@ class Player {
     }
 }
 
+class Projectile {
+    constructor(x, y, radius, colour, velocity) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.colour = colour;
+        this.velocity = velocity;
+    }
+
+    draw() {
+        ctx.beginPath();
+        ctx.arc(this.x , this.y, this.radius, 0, Math.PI * 2, false);
+        ctx.fillStyle = this.colour;
+        ctx.fill();
+    }
+
+    update(){
+        this.draw();
+        this.x = this.x + this.velocity.x;
+        this.y = this.y + this.velocity.y;
+    }
+}
+
 const x_mid = canvas.width / 2;
 const y_mid = canvas.height / 2;
-
 const player = new Player(x_mid, y_mid, 30, 'blue', 10);
-player.draw();
+
+const projectiles = [];
+
+function animate() {
+    requestAnimationFrame(animate)
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    player.draw();
+    projectiles.forEach(projectile =>{
+        projectile.update();
+    })
+}
+
+// window???
+window.addEventListener('click', (event) => {
+    const angle = Math.atan2(event.clientY - player.y, 
+                            event.clientX - player.x);
+    const velocity = {
+        x: Math.cos(angle),
+        y: Math.sin(angle),
+    }
+    var dir_x = 0
+    projectiles.push(new Projectile(
+        player.x,
+        player.y,
+        5, 
+        'red', 
+        velocity,
+    ));
+})
+
+
+animate()
