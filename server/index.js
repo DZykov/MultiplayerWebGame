462e77f4e72e97cb058a11dbeb309cb7d4cbcd53
DZykov <dzykov@protonmail.com>
2022-09-02 17:58:31 -0400

    update for deployment

--- server/index.js
+++ server/index.js
@@ -170,6 +170,6 @@ io.on('connection', (socket) => {
     });
 });
 
-server.listen(process.env.port || 3000, () => {
+server.listen(process.env.PORT || 3000, () => {
     console.log('Server is running!');
 });
